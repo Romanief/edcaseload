7c725a93881df32f159f9f06acaed4b6b6c460e2
Romanief <federicoromaniello@outlook.com>
2024-06-16 13:01:30 +0100

    Changed settings for vercel deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('edcaseload/', include("edcaseload.urls")),
+    path('', include("edcaseload.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
